aa08ed08f2d76ef3d79840e0706d088d838c6177
Vansh <44532230+aroravansh@users.noreply.github.com>
2021-06-19 19:33:54 +0530

    Two Sum
    Runtime: 12 ms
    Memory Usage: 12 MB

--- Leetcode/Arrays/1. TWOSUMS.cpp
+++ Leetcode/Arrays/1. TWOSUMS.cpp
@@ -6,7 +6,6 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1]
 */
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
